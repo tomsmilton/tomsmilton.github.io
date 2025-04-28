90084432dd31a369cdf923b46c2625d52b76d825
Tom <tom.milton@amododesign.com>
2025-04-28 23:53:34 +0100

    bad fixes to formatting

---
+++ nav.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Create the navigation element
+    const nav = document.createElement('nav');
+    nav.className = 'site-nav';
+    nav.innerHTML = `
+        <ul>
+            <li><a style="font-weight: 100; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif">tsmilton</a></li>
+            <li><a href="index.html">Home</a></li>
+        </ul>
+    `;
+
+    // Find the article element and insert the navigation before it
+    const article = document.querySelector('article');
+    if (article) {
+        article.parentNode.insertBefore(nav, article);
+    } else {
+        console.warn('No article element found to insert navigation before');
+    }
+}); 
